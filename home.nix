c8e306121cff0b58255bb8b3a4e84405fcfc3ace
Adrian Forsius <adrianforsius@gmail.com>
2024-01-24 10:42:39 +0100

    Move editorconfig to its own file

--- home.nix
+++ home.nix
@@ -21,58 +21,7 @@
   # environment.
 
   home.packages = import ./home/packages.nix { inherit pkgs config; };
-
-  editorconfig = {
-    enable = true;
-    settings = {
-      "*" = {
-        charset = "utf-8";
-
-        end_of_line = "LF";
-        insert_final_newline = true;
-        trim_trailing_whitespace = true;
-
-        indent_style = "space";
-        indent_size = 4;
-      };
-
-      "Makefile" = {
-        indent_style = "tab";
-      };
-
-      "*.go" = {
-        indent_style = "tab";
-        indent_size = 8;
-      };
-
-      ".vimrc" = {
-        indent_style = "tab";
-        indent_size = 2;
-      };
-
-      ".*" = {
-        indent_style = "space";
-        indent_size = 2;
-      };
-
-      "*.sh" = {
-        indent_style = "space";
-        indent_size = 2;
-      };
-
-      "[{*.json,*.js,*.ts}]" = {
-        indent_style = "space";
-        indent_size = 2;
-      };
-
-      "[{*.yaml,*.yml}]" = {
-        indent_style = "space";
-        indent_size = 2;
-      };
-    };
-  };
-
-
+  editorconfig = import ./home/editorconfig.nix { };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through "home.file".
---
+++ home/editorconfig.nix
@@ -0,0 +1,50 @@
+{ ... }:
+{
+  enable = true;
+  settings = {
+    "*" = {
+      charset = "utf-8";
+
+      end_of_line = "LF";
+      insert_final_newline = true;
+      trim_trailing_whitespace = true;
+
+      indent_style = "space";
+      indent_size = 4;
+    };
+
+    "Makefile" = {
+      indent_style = "tab";
+    };
+
+    "*.go" = {
+      indent_style = "tab";
+      indent_size = 8;
+    };
+
+    ".vimrc" = {
+      indent_style = "tab";
+      indent_size = 2;
+    };
+
+    ".*" = {
+      indent_style = "space";
+      indent_size = 2;
+    };
+
+    "*.sh" = {
+      indent_style = "space";
+      indent_size = 2;
+    };
+
+    "[{*.json,*.js,*.ts}]" = {
+      indent_style = "space";
+      indent_size = 2;
+    };
+
+    "[{*.yaml,*.yml}]" = {
+      indent_style = "space";
+      indent_size = 2;
+    };
+  };
+}
