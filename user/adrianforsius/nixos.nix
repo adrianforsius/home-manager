fad8edd712a70c9aa69eaf9442491db2312f7595
Adrian Forsius <adrianforsius@gmail.com>
2024-02-09 09:46:49 +0100

    Add with pkgs for nixos confi

--- user/adrianforsius/nixos.nix
+++ user/adrianforsius/nixos.nix
@@ -17,16 +17,17 @@
     hashedPassword = "$6$jM/t2HIzfLCDj4mn$5PNkU9y3JsRxafqJ9X.l4q4AtAPcQgPBf8dzcmulVsSkZO9rE1CxtmBuJebbWwI3Til.wAyW/PahSifdQTpYh1";
   };
 
-  home.packages = lib.mkAfter [
-    teams-for-linux
-    whatsapp-for-linux
-    xclip
-    # xfce.xfce4-terminal # TODO: fix when installed from nix icons gets messed up
-    pacman
-    # albert
-    (callPackage ./home/package/cerebro {})
-    virtualbox
-  ];
+  home.packages = with pkgs;
+    lib.mkAfter [
+      teams-for-linux
+      whatsapp-for-linux
+      xclip
+      # xfce.xfce4-terminal # TODO: fix when installed from nix icons gets messed up
+      pacman
+      # albert
+      (callPackage ./home/package/cerebro {})
+      virtualbox
+    ];
 
   # home.sessionVariables.EDITOR = lib.mkForce "nano";
 }
