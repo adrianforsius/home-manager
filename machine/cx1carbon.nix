e0f136f4ccab33f5a78f7566ca6f45915374bd99
Adrian Forsius <adrianforsius@gmail.com>
2024-12-15 21:41:41 +0100

    Triage tlp

--- machine/cx1carbon.nix
+++ machine/cx1carbon.nix
@@ -315,52 +315,53 @@
     enable = true;
   };
   services.fwupd.enable = true; # firmware update; run: sudo fwupdmgr update
+  services.tlp.enable = true; # powermanagement helper
 
   # Allow closing lid without going to sleep
   # services.logind.lidSwitch = "ignore";
-  services.logind.lidSwitch = "suspend-then-hibernate";
-
-  systemd.services."awake-after-suspend-for-a-time" = {
-    description = "Sets up the suspend so that it'll wake for hibernation only if not on AC power";
-    wantedBy = ["suspend.target"];
-    before = ["systemd-suspend.service"];
-    environment = {
-      HIBERNATE_SECONDS = "10";
-      HIBERNATE_LOCK = "/var/run/autohibernate.lock";
-    };
-    script = ''
-      if [ $(cat /sys/class/power_supply/AC/online) -eq 0 ]; then
-        curtime=$(date +%s)
-        echo "$curtime $1" >> /tmp/autohibernate.log
-        echo "$curtime" > $HIBERNATE_LOCK
-        ${pkgs.utillinux}/bin/rtcwake -m no -s $HIBERNATE_SECONDS
-      else
-        echo "System is on AC power, skipping wake-up scheduling for hibernation." >> /tmp/autohibernate.log
-      fi
-    '';
-    serviceConfig.Type = "simple";
-  };
-
-  systemd.services."hibernate-after-recovery" = {
-    description = "Hibernates after a suspend recovery due to timeout";
-    wantedBy = ["suspend.target"];
-    after = ["systemd-suspend.service"];
-    environment = {
-      HIBERNATE_SECONDS = "10";
-      HIBERNATE_LOCK = "/var/run/autohibernate.lock";
-    };
-    script = ''
-      curtime=$(date +%s)
-      sustime=$(cat $HIBERNATE_LOCK)
-      rm $HIBERNATE_LOCK
-      if [ $(($curtime - $sustime)) -ge $HIBERNATE_SECONDS ] ; then
-        systemctl hibernate
-      else
-        ${pkgs.utillinux}/bin/rtcwake -m no -s 1
-      fi
-    '';
-    serviceConfig.Type = "simple";
-  };
+  # services.logind.lidSwitch = "suspend-then-hibernate";
+  #
+  # systemd.services."awake-after-suspend-for-a-time" = {
+  #   description = "Sets up the suspend so that it'll wake for hibernation only if not on AC power";
+  #   wantedBy = ["suspend.target"];
+  #   before = ["systemd-suspend.service"];
+  #   environment = {
+  #     HIBERNATE_SECONDS = "10";
+  #     HIBERNATE_LOCK = "/var/run/autohibernate.lock";
+  #   };
+  #   script = ''
+  #     if [ $(cat /sys/class/power_supply/AC/online) -eq 0 ]; then
+  #       curtime=$(date +%s)
+  #       echo "$curtime $1" >> /tmp/autohibernate.log
+  #       echo "$curtime" > $HIBERNATE_LOCK
+  #       ${pkgs.utillinux}/bin/rtcwake -m no -s $HIBERNATE_SECONDS
+  #     else
+  #       echo "System is on AC power, skipping wake-up scheduling for hibernation." >> /tmp/autohibernate.log
+  #     fi
+  #   '';
+  #   serviceConfig.Type = "simple";
+  # };
+  #
+  # systemd.services."hibernate-after-recovery" = {
+  #   description = "Hibernates after a suspend recovery due to timeout";
+  #   wantedBy = ["suspend.target"];
+  #   after = ["systemd-suspend.service"];
+  #   environment = {
+  #     HIBERNATE_SECONDS = "10";
+  #     HIBERNATE_LOCK = "/var/run/autohibernate.lock";
+  #   };
+  #   script = ''
+  #     curtime=$(date +%s)
+  #     sustime=$(cat $HIBERNATE_LOCK)
+  #     rm $HIBERNATE_LOCK
+  #     if [ $(($curtime - $sustime)) -ge $HIBERNATE_SECONDS ] ; then
+  #       systemctl hibernate
+  #     else
+  #       ${pkgs.utillinux}/bin/rtcwake -m no -s 1
+  #     fi
+  #   '';
+  #   serviceConfig.Type = "simple";
+  # };
 
   networking = {
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
